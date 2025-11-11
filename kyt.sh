fab9a9c1d323ccab543058a4b132c1086067d69d
Anggabayuwijaya599 <anggabayuwijaya599@gmail.com>
2025-11-11 21:44:45 +0700

    Update kyt.sh

--- kyt.sh
+++ kyt.sh
@@ -9,13 +9,13 @@ NC='\e[0m'
 apt update && apt upgrade
 apt install python3 python3-pip git
 cd /usr/bin
-wget https://github.com/hokagelegend9999/force/raw/refs/heads/main/limit/bot.zip
+wget https://raw.githubusercontent.com/Anggabayuwijaya599/singgahvpn/refs/heads/main/limit/bot.zip
 unzip bot.zip
 mv bot/* /usr/bin
 chmod +x /usr/bin/*
 rm -rf bot.zip
 clear
-wget https://github.com/hokagelegend9999/force/raw/refs/heads/main/limit/kyt.zip
+wget https://raw.githubusercontent.com/Anggabayuwijaya599/singgahvpn/refs/heads/main/limit/kyt.zip
 unzip kyt.zip
 pip3 install -r kyt/requirements.txt
 
